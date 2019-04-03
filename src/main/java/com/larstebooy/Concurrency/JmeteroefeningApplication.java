d7d334418cf02a3270cf093d5dafe07913923f8a
Lars <lars.tebooy@hotmail.com>
2019-04-03 11:53:21 +0200

    added concurrency

---
+++ src/main/java/com/larstebooy/Concurrency/JmeteroefeningApplication.java
@@ -0,0 +1,13 @@
+package com.larstebooy.Concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeteroefeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmeteroefeningApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/larstebooy/Concurrency/controller/RestController.java
@@ -0,0 +1,39 @@
+package com.larstebooy.Concurrency.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    private int counter = 0;
+    private Lock lock = new ReentrantLock();
+
+    @PostMapping("/counter/inc")
+    public int addToCounter() throws InterruptedException {
+        if(lock.tryLock(5, TimeUnit.SECONDS)){
+            try{
+                counter++;
+            }finally {
+                lock.unlock();
+            }
+        }
+        return counter;
+    }
+
+    @GetMapping("/counter")
+    public int showCounter(){
+        return counter;
+    }
+
+    @PostMapping("/counter/reset")
+    public int resetCounter(){
+        counter = 0;
+        return counter;
+    }
+
+}
